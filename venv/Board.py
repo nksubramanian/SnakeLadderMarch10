103d92db664f4e7ede5d62d23e8e2c5325d5d53b
subramaniannk <58900536+subramaniannk@users.noreply.github.com>
2021-03-11 18:27:57 +0530

    made constants

--- venv/Board.py
+++ venv/Board.py
@@ -1,5 +1,8 @@
 from GameStatus import GameStatus
 class Board:
+    _fine_unable_to_move = 10
+    _reward_for_ladder = 100
+    _fine_for_snake = 100
     def __init__(self, last_position, snake, ladder):
         self.last_position = last_position
         self._all_position = set(range(1, last_position + 1))
@@ -9,13 +12,13 @@ class Board:
 
     def Play(self, player, diceroll):
         if (player._position + diceroll) not in self._all_position:
-            player.RemoveMoney(10)
+            player.RemoveMoney(self._fine_unable_to_move)
         elif (player._position + diceroll) in self._snake:
             player.MoveTo(self._snake[player._position + diceroll])
-            player.RemoveMoney(100)
+            player.RemoveMoney(self._fine_for_snake)
         elif (player._position + diceroll) in self._ladder:
             player.MoveTo(self._ladder[player._position + diceroll])
-            player.AddMoney(100)
+            player.AddMoney(self._reward_for_ladder)
         else:
             player.MoveTo(player._position + diceroll)
 
